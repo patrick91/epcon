5877f87e3ae80bcfabbdc78f24dae04e32b66c48
Marco Acierno <marcoacierno96@gmail.com>
2016-11-09 23:29:43 +0100

    Add page class init in go_to_page background step

--- tests/integration_tests/steps/page_steps.py
+++ tests/integration_tests/steps/page_steps.py
@@ -9,10 +9,10 @@ AVAILABLE_PAGES = {
 
 
 @given('the user is on the {page} page')
-def go_to_page(context, page):
+def go_to_page(context, page, live_server, selenium):
     if page not in AVAILABLE_PAGES:
         # Warn about the invalid page?
         raise KeyError('Page {} is not a valid page. Valid pages: {}'.format(page, AVAILABLE_PAGES))
 
     page_cls = AVAILABLE_PAGES[page]
-    context.page = page_cls(base_url=)
+    context.page = page_cls(base_url=live_server.url, selenium=selenium)
